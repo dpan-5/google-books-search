799a346cdebfbd5f274421d1e857e3aa29b91eae
David Pan <dpan5425@gmail.com>
2020-11-05 10:44:55 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Search from './pages/Search';
+import Saved from './pages/Saved';
+
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
+    </Router>
   );
 }
 
