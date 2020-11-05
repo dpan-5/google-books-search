00015b215e14e2d8341c55a1d5d8b2041c4cf307
David Pan <dpan5425@gmail.com>
2020-11-05 11:04:25 -0500

    added search bar to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,24 @@
 export default function Search() {
-    return (
-        <h1>Search goes here</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-8 offset-sm-2">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Book Search</label>
+              <input
+                type="text"
+                class="form-control"
+                id="bookSearchInput"
+                aria-describedby="bookSearch"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
